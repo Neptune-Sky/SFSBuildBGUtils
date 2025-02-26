c46e8ecde223adc9af6cb02b0f11e9b508e8657b
Neptune-Sky <neptuneskies@proton.me>
2025-02-25 21:31:41 -0600

    create repobecause yes

---
+++ src/CustomKeybindings.cs
@@ -0,0 +1,47 @@
+using HarmonyLib;
+using ModLoader;
+using ModLoader.Helpers;
+using SFS;
+using SFS.Audio;
+using SFS.UI;
+using SFS.World;
+using UnityEngine;
+using static SFS.Input.KeybindingsPC;
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace SFSBuildBGUtils
+{
+    public class DefaultKeys
+    {
+        public Key ShowMenu = KeyCode.Slash;
+    }
+
+    public class BGU_Keybindings : ModKeybindings
+    {
+        private static readonly DefaultKeys defaultKeys = new();
+
+        #region Keys
+        public Key ShowMenu = defaultKeys.ShowMenu;
+        #endregion
+
+        public static BGU_Keybindings main;
+
+        public static void LoadKeybindings()
+        {
+            main = SetupKeybindings<BGU_Keybindings>(Main.main);
+            SceneHelper.OnBuildSceneLoaded += OnBuildLoad;
+        }
+
+        private static void OnBuildLoad()
+        {
+            AddOnKeyDown_Build(main.ShowMenu, () => GUI.CustomRotate(true));
+        }
+
+        public override void CreateUI()
+        {
+            CreateUI_Text("Build BG Keybindings");
+            CreateUI_Keybinding(ShowMenu, defaultKeys.ShowMenu, "Show Options Menu");
+            CreateUI_Space();
+        }
+    }
+}
---
+++ src/Main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using HarmonyLib;
+using JetBrains.Annotations;
+using ModLoader;
+using ModLoader.Helpers;
+using SFS.Input;
+using SFS.IO;
+using SFS.UI;
+using UITools;
+
+namespace SFSBuildBGUtils
+{
+    [UsedImplicitly]
+    public class Main : Mod, IUpdatable
+    {
+        public override string ModNameID => "BuildSettings";
+        public override string DisplayName => "Build Settings";
+        public override string Author => "StarMods";
+        public override string MinimumGameVersionNecessary => "1.5.10.2";
+        public override string ModVersion => "v2.1.5.1";
+        public override string Description => "Build settings window and various changes to build mode. See the GitHub repository for a full list of features.";
+        public override Dictionary<string, string> Dependencies { get; } = new() { { "UITools", "1.0" } };
+
+        public override Action LoadKeybindings => BGU_Keybindings.LoadKeybindings;
+        public Dictionary<string, FilePath> UpdatableFiles => new() { { "https://github.com/Neptune-Sky/SFSBuildSettings/releases/latest/download/BuildSettings.dll", new FolderPath(ModFolder).ExtendToFile("BuildSettings.dll") } };
+
+        private static Harmony patcher;
+
+        public static Main main;
+        public static FolderPath modFolder;
+
+        public override void Early_Load()
+        {
+            modFolder = new FolderPath(ModFolder);
+
+            main = this;
+
+            patcher = new Harmony("Neptune.BuildBGUtils.Mod");
+            patcher.PatchAll();
+        }
+
+        public override void Load()
+        {
+            Config.Setup();
+            SceneHelper.OnBuildSceneLoaded += () =>
+            {
+                GUI.Setup();
+                SkinUnlocker.Initialize();
+            };
+        }
+    }
+}
